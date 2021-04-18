d9881a45acd2c2ddd2d320baa219595ccdb39044
shahruz <hello@shahr.uz>
2021-04-17 23:39:17 -0700

    cors fix, upload json

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import IPFS from 'ipfs';
 import polka from 'polka';
 import fileUpload from 'express-fileupload';
+import bodyparser from 'body-parser';
+import cors from 'cors';
 
 let node;
 IPFS.create().then(_node => {
@@ -8,13 +10,21 @@ IPFS.create().then(_node => {
 });
 
 polka()
-  .use(fileUpload())
+  .use(cors(), fileUpload(), bodyparser.json())
   .use((_req, res, next) => {
     res.json = data => {
       res.setHeader('Content-Type', 'application/json');
       res.end(JSON.stringify(data));
     };
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, Content-Type, X-Auth-Token'
+    );
     next();
   })
   .get('/ipfs/:hash', async (req, res) => {
@@ -31,5 +41,10 @@ polka()
   .post('/upload', async (req, res) => {
     const data = await node.add(req.files.files.data);
     return res.json({ hash: data.path });
+  })
+  .post('/uploadJSON', async (req, res) => {
+    const data = req.body;
+    const { cid } = await node.add(JSON.stringify(data));
+    return res.json({ hash: cid });
   })
   .listen(3000);
