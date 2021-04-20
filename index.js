019d26097ee7e587453c4989cddd04c4fd89e0dc
shahruz <hello@shahr.uz>
2021-04-19 23:13:23 -0700

    simplify

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-import IPFS from 'ipfs';
-import polka from 'polka';
-import fileUpload from 'express-fileupload';
-import bodyparser from 'body-parser';
-import cors from 'cors';
-import unzipper from 'unzipper';
-import fetch from 'node-fetch';
+const IPFS = require('ipfs');
+const polka = require('polka');
+const fileUpload = require('express-fileupload');
+const bodyparser = require('body-parser');
 
 let node;
 IPFS.create().then(_node => {
@@ -12,7 +9,7 @@ IPFS.create().then(_node => {
 });
 
 polka()
-  .use(cors(), fileUpload(), bodyparser.json())
+  .use(fileUpload(), bodyparser.json())
   .use((_req, res, next) => {
     res.json = data => {
       res.setHeader('Content-Type', 'application/json');
@@ -38,24 +35,4 @@ polka()
     const data = await node.add(JSON.stringify(body));
     return res.json({ hash: data.path });
   })
-  .get('/uploadSite', async (req, res) => {
-    const { buildURL } = req.query;
-    const buffer = await fetch(buildURL).then(res => res.buffer());
-    const directory = await unzipper.Open.buffer(buffer);
-    const files = await Promise.all(
-      directory.files.map(async file => {
-        return {
-          path: file.path.replace('out/', 'site/'),
-          content: await file.buffer()
-        };
-      })
-    );
-    const results = [];
-    for await (const result of node.addAll(files)) {
-      results.push(result);
-    }
-    return res.json({
-      url: `ipfs://${results[results.length - 1].cid}`
-    });
-  })
-  .listen(3000);
+  .listen(process.env.PORT || 3000);
